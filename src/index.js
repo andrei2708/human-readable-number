4ef00d39b5e90f1e12edd58bca1f160c9a49601e
Andrei Pauliukovich <apavlukovich@gmail.com>
2021-03-11 21:34:15 +0300

    feat: colution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
+    let result = [],
+    str = number.toString(),
+    length = str.length,
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+    tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+    badTens = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],  //для определения десятков в трехразрядных числах
+    dozens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (length == 2 && number < 20) return tens[str[1]];
+    if (number === 0) 
+    return 'zero';
   
-}
+    length === 1 ? result.push(units[str[0]]) :
+    length === 2 ? result.push(dozens[str[0]], units[str[1]]) :
+      (length === 3 && (10 < str.slice(-2) && str.slice(-2) < 20)) ? result.push(units[str[0]], 'hundred', badTens[str[2]]) : //для определения десятков в трехразрядных числах
+        result.push(units[str[0]], 'hundred', dozens[str[1]], units[str[2]]);
+
+  return result.join(" ").replace(/\s+/g, ' ').trim(); //regExp для удаления двойных пробелов, они появляются, если есть нуль в середине числа
+    }
+    
+    
+    
+   /* length === 1 ? result.push(units[str[0]]) :
+    length === 2 ? result.push(dozens[str[0]], units[str[1]]) :
+      (length === 3 && (10 < str.slice(-2) && str.slice(-2) < 20)) ? result.push(units[str[0]], 'hundred', badTens[str[2]]) : //для определения десятков в трехразрядных числах
+        result.push(units[str[0]], 'hundred', dozens[str[1]], units[str[2]]);
+
+  return result.join(" ").replace(/\s+/g, ' ').trim();*/
+
